debd6d89f4f990a898475c00840424e3378647ee
Wu tung yu <wty262690@gmail.com>
2024-10-27 03:12:37 +0800

    clean code

--- translate/speech.js
+++ translate/speech.js
@@ -1,94 +1,104 @@
-var infoBox; 
-var tempBox; 
-var chineseBox; //中文
+var infoBox, tempBox, chineseBox; 
 var startStopButton; 
 var recognizing = false; 
+var recognition;
 var mouth = ['','ㄅ','ㄅ','ㄆ','ㄆ','ㄇ','ㄇ','ㄈ','ㄈ','ㄉ','ㄉ','ㄊ','ㄊ','ㄋ','ㄋ','ㄌ','ㄌ','ㄍ','ㄎ','ㄏ','ㄐ','ㄑ','ㄒ','ㄓ','ㄔ','ㄕ','ㄖ','ㄗ','ㄘ','ㄙ','ㄚ','ㄛ','ㄜ','ㄝ','ㄞ','ㄞ','ㄟ','ㄟ','ㄠ','ㄠ','ㄡ','ㄡ','ㄢ','ㄢ','ㄣ','ㄣ','ㄤ','ㄤ','ㄥ','ㄥ','ㄦ','ㄦ','ㄧ','ㄨ','ㄩ'];
 
 $(document).ready(function () {
-  console.log("Hello World!");
-});
-
-function startButton(event) {
   infoBox = document.getElementById("infoBox");
   tempBox = document.getElementById("tempBox"); 
   chineseBox = document.getElementById("chineseBox"); 
   startStopButton = document.getElementById("startStopButton"); 
-  langCombo = document.getElementById("langCombo"); 
-  if (recognizing) { 
-    tempBox.innerHTML = '...'; 
-    recognition.stop();
-  } else { 
-    tempBox.innerHTML = '...請開始說話...'; 
+
+  /*確認瀏覽器是否支援API*/
+  if (!('webkitSpeechRecognition' in window)) {  
+    infoBox.innerText = "本瀏覽器不支援語音辨識，請更換瀏覽器！(Chrome 25 版以上才支援語音辨識)";
+  } 
+  if (!('speechSynthesis' in window)) {
+    infoBox.innerText = "本瀏覽器不支援語音合成，請更換瀏覽器！(Chrome 33 版以上才支援語音辨識)";
+  }
+  else {
+    //錄音設置
+    recognition = new webkitSpeechRecognition(); 
+    recognition.continuous = true;
+    recognition.interimResults = true;
     recognition.lang = 'cmn-Hant-TW';
+  
+    //錄製開始
+    recognition.onstart = function() { 
+      recognizing = true; 
+      startStopButton.value = "stop";  
+      tempBox.innerHTML = '...請開始說話...'; 
+    };
+  
+    //錄製停止
+    recognition.onend = function() { 
+      recognizing = false;
+      startStopButton.value = "start";  
+      tempBox.innerHTML = '...'; 
+    };
+  
+    //錄製結果
+    recognition.onresult = function(event) {
+      var interim_transcript = ''; 
+      for (var i = event.resultIndex; i < event.results.length; ++i) {
+        interim_transcript += event.results[i][0].transcript;
+      }
+      if (interim_transcript.trim().length > 0) 
+          tempBox.innerText = interim_transcript; //中文
+          chineseBox.innerText = pinyinUtil.getPinyin(interim_transcript); //注音
+    };
+  }
+});
+
+/*錄音按鈕*/ 
+function startButton(event) {
+  if (recognizing) { 
+    //停止錄製
+    recognition.stop();
+  } 
+  else { 
+    //開始錄製
     recognition.start();
   }
 }
 
-if (!('webkitSpeechRecognition' in window)) {  
-  infoBox.innerText = "本瀏覽器不支援語音辨識，請更換瀏覽器！(Chrome 25 版以上才支援語音辨識)";
-} 
-if (!('speechSynthesis' in window)) {
-  infoBox.innerText = "本瀏覽器不支援語音合成，請更換瀏覽器！(Chrome 33 版以上才支援語音辨識)";
-}
-else {
-  var recognition = new webkitSpeechRecognition(); 
-  recognition.continuous = true;
-  recognition.interimResults = true;
 
-  recognition.onstart = function() { 
-    recognizing = true; 
-    startStopButton.value = "stop"; 
-    //infoBox.innerText = "...請開始說話...";  
-  };
-
-  recognition.onend = function() { 
-    recognizing = false;
-    startStopButton.value = "start";  
-    tempBox.innerHTML = '...'; 
-    //infoBox.innerText = "支援"; 
-  };
-
-  recognition.onresult = function(event) {
-    var interim_transcript = ''; 
-    for (var i = event.resultIndex; i < event.results.length; ++i) {
-      interim_transcript += event.results[i][0].transcript;
-    }
-    if (interim_transcript.trim().length > 0) 
-        tempBox.innerText = interim_transcript;
-        var chinese = pinyinUtil.getPinyin(interim_transcript);
-        chineseBox.innerText = chinese;
-  };
-}
 function speakButton(event) {
+  //動畫設置
   let vid = document.getElementById("mouthvideo");
   vid.playbackRate = 10;
+  
+  //語音合成
   var msg = new SpeechSynthesisUtterance('語音輸入測試');
   window.speechSynthesis.speak(msg);
+  
+  //語音轉注音
   var chinese = pinyinUtil.getPinyin('語音輸入測試');
-  chineseBox = document.getElementById("chineseBox");
+
   console.log(chinese);
   chineseBox.innerText = chinese;
   chinese = new String(chinese).split(' ')
-  console.log(chinese);
   var index = 0;
-        let speakanimation = setInterval(() => {
-            if (index >= chinese.length-1)clearInterval(speakanimation);
-            //for (var i = 0; i< chinese[index].length; i++){
-              switch(chinese[index][0]){
-                case' ': case'ˋ': case'ˇ': case'ˊ': case'ˉ': break;
-                case undefined:    
-                  mouthdom.src = "./image/mouth-1.png";
-                  break;
-                default:
-                  changeimg(chinese,index,0);
-              }
-            //}
-            index++;
-        }, 250);
+
+  /*動畫播放__需要重寫*/ 
+  let speakanimation = setInterval(() => {
+      if (index >= chinese.length-1)clearInterval(speakanimation);
+      //for (var i = 0; i< chinese[index].length; i++){
+        switch(chinese[index][0]){
+          case' ': case'ˋ': case'ˇ': case'ˊ': case'ˉ': break;
+          case undefined:    
+            mouthdom.src = "./image/mouth-1.png";
+            break;
+          default:
+            changeimg(chinese,index,0);
+        }
+      //}
+      index++;
+  }, 250);
 }
 
-var changeimg = function (chinese,index,i){
+function changeimg(chinese,index,i){
   var mouthdom = document.getElementById("mouth");
   var mouthvideo = document.getElementById("mouthvideo");
   console.log(chinese[index][i] ,mouth.indexOf(chinese[index][i]));
